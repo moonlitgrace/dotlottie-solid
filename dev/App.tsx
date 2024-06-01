e9966aa3c7944618e43ef82730a52afe5b02dbee
tokitouq <suneeth.since2005@gmail.com>
2024-06-01 16:32:44 +0530

    format

--- dev/App.tsx
+++ dev/App.tsx
@@ -1,10 +1,7 @@
 import type { Component } from 'solid-js';
-import { Hello } from 'src';
 
 const App: Component = () => {
-  return (
-    <span>Hello</span>
-  )
+	return <span>Hello</span>;
 };
 
 export default App;
