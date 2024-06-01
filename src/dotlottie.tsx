3c6e4c7111598ee2446dfb54d858ac1d162589e4
tokitouq <suneeth.since2005@gmail.com>
2024-06-01 23:22:14 +0530

    fix: linting and format

--- src/dotlottie.tsx
+++ src/dotlottie.tsx
@@ -1,31 +1,31 @@
-import type { DotLottie, Config } from '@lottiefiles/dotlottie-web';
+import type { Config } from '@lottiefiles/dotlottie-web';
 
 import { useDotLottie } from './use-dotlottie';
 import { ComponentProps, JSX, splitProps } from 'solid-js';
 
 export type DotLottieSolidProps = Omit<Config, 'canvas'> &
-  	ComponentProps<'canvas'> & Partial<{
-    	autoResizeCanvas?: boolean;
-    	playOnHover?: boolean;
-  	}>;
+	ComponentProps<'canvas'> &
+	Partial<{
+		autoResizeCanvas?: boolean;
+		playOnHover?: boolean;
+	}>;
 
 export const DotLottieSolid = (props: DotLottieSolidProps): JSX.Element => {
-	const [dotLottieProps, restProps] = splitProps(
-		props, [
-			"src",
-			"data",
-			"mode", 
-			"loop",
-			"speed",
-			"marker",
-			"segment",
-			"autoplay",
-			"playOnHover",
-			"renderConfig",
-			"autoResizeCanvas",
-			"useFrameInterpolation"
-		]);
+	const [dotLottieProps, restProps] = splitProps(props, [
+		'src',
+		'data',
+		'mode',
+		'loop',
+		'speed',
+		'marker',
+		'segment',
+		'autoplay',
+		'playOnHover',
+		'renderConfig',
+		'autoResizeCanvas',
+		'useFrameInterpolation',
+	]);
 	const { DotLottieComponent } = useDotLottie(dotLottieProps);
 
-	return <DotLottieComponent {...restProps} />
+	return <DotLottieComponent {...restProps} />;
 };
